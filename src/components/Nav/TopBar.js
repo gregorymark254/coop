02cf38b4ce6b37baf216b42c1840dea002534ea1
Gregory Mark <gmark@onfonmedia.com>
2024-11-27 15:30:43 +0300

    coop

--- src/components/Nav/TopBar.js
+++ src/components/Nav/TopBar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { useStateContext } from '../Context/ContextProvider';
-import { MdOutlineKeyboardArrowDown, MdLogout } from 'react-icons/md';
+import { MdLogout } from "react-icons/md";
 
 
 const Navbar = () => {
@@ -39,13 +39,14 @@ const Navbar = () => {
 
   return (
     <main className='topnav'>
-      <div className='flex justify-between items-center p-1 shadow-md'>
-        <div className='text-xl p-2 hover:cursor-pointer'>
-          <button className='text-[#0E6F1E]' onClick={handleActiveMenu}><AiOutlineMenu /></button>
+      <div className='flex justify-between items-center p-4 shadow-md text-white'>
+        <div className='text-xl p-2 hover:cursor-pointer flex items-center space-x-4'>
+          <button className='text-white' onClick={handleActiveMenu}><AiOutlineMenu /></button>
+          <h3><b>Inua Mkulima Subsidy Program</b></h3>
         </div>
-        <div className='flex space-x-5'>
-          <h4><b>Logged In As: KIMATHI</b></h4>
-          <button onClick={signOut} className=''>Logout</button>
+        <div className='flex items-center space-x-5'>
+          <h5><b>Logged In As: KIMATHI</b></h5>
+          <button onClick={signOut} className='border border-white rounded-md px-4 py-1 flex items-center gap-1'><span><MdLogout /></span>Logout</button>
         </div>
       </div>
     </main>
